25461fd4e427a38697d5d0de656e149edd237a97
rzwm <279428101@qq.com>
2017-02-12 15:56:00 +0800

    Add first project - 1.EmptyWindow

---
+++ src/1.EmptyWindow/EmptyWindow.cpp
@@ -0,0 +1,34 @@
+﻿#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include <iostream>
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(512, 512);
+	glutInitWindowPosition(100, 100);
+	glutInitContextVersion(4, 3);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutCreateWindow(argv[0]);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Unable to initialize GLEW ... exiting" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	glutDisplayFunc(display);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glutMainLoop();
+
+	return 0;
+}
