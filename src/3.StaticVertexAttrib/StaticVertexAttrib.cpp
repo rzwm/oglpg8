ed1177d1c45d330d31b93ee946f44639c5d43f90
rzwm <279428101@qq.com>
2017-02-16 21:56:04 +0800

    添加3.StaticVertexAttrib项目

---
+++ src/3.StaticVertexAttrib/StaticVertexAttrib.cpp
@@ -0,0 +1,111 @@
+﻿///////////////////////////////////////////////
+// 简介：
+//		试验静态顶点属性。使用静态顶点属性设置颜
+//		色值，在屏幕上绘制一个正方形。
+//
+///////////////////////////////////////////////
+#include "GL/glew.h"
+#include "GL/freeglut.h"
+#include <iostream>
+#include "LoadShaders.h"
+
+GLuint VAO;
+GLuint VBO;
+GLuint g_program;
+
+void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	if (g_program != 0)
+	{
+		glBindVertexArray(VAO);
+		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, (const void*)0);
+		glEnableVertexAttribArray(0);
+
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+		glDisableVertexAttribArray(0);
+	}
+
+	glutSwapBuffers();
+}
+
+void init()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	const GLfloat vertices[] =
+	{
+		-0.9f, -0.9f,
+		 0.9f, -0.9f,
+		-0.9f,  0.9f,
+		 0.9f,  0.9f
+	};
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	ShaderInfo shaders[] =
+	{
+		{ GL_VERTEX_SHADER, "StaticVertexAttrib.vs" },
+		{ GL_FRAGMENT_SHADER, "StaticVertexAttrib.fs" },
+		{ GL_NONE, "" }
+	};
+	g_program = loadShaders(shaders);
+	if (g_program != 0)
+	{
+		glUseProgram(g_program);
+	}
+
+	// 设置静态顶点属性颜色为红色
+	glVertexAttrib4f(1, 1.0f, 0.0f, 0.0f, 1.0f);
+}
+
+void uninit()
+{
+	if (g_program != 0)
+	{
+		glDeleteProgram(g_program);
+		g_program = 0;
+	}
+
+	glDeleteBuffers(1, &VBO);
+	VBO = 0;
+
+	glDeleteVertexArrays(1, &VAO);
+	VAO = 0;
+}
+
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA);
+	glutInitWindowSize(512, 512);
+	glutInitWindowPosition(100, 100);
+	glutInitContextVersion(4, 3);
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	glutCreateWindow(argv[0]);
+
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Unable to initialize GLEW ... exiting" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+
+	glutDisplayFunc(display);
+
+	init();
+
+	glutMainLoop();
+
+	uninit();
+
+	return 0;
+}
